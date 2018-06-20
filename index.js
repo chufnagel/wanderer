d122237edc2bbe9ae34280d1c0e3c104bee27dc8
Charles <charles.hufnagel@protonmail.com>
2018-06-20 11:32:56 -0400

    Add cross-env and parcel config

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import logo from './logo.svg';
+import './index.css';
+
+const App = () (
+  <div className="App">>
+  <h1>Parcel Test</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// Hot Module Replacement
+if (module.hot) {
+  module.hot.accept();
+};
