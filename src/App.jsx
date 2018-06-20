57d65757553e7f00673b862686c8db9e3ee94343
Charles <charles.hufnagel@protonmail.com>
2018-06-20 15:37:57 -0400

    Successfully configure parcel.js

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App-Container">
+        <h1 className="app-title">Parcel Test</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
