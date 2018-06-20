896c35a68aed6bfac02cc0d366f455d0837356cf
Charles <charles.hufnagel@protonmail.com>
2018-06-20 15:48:40 -0400

    Add service worker, tweak linter, add testing packages to package.json

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
 ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
 
 // Hot Module Replacement
 if (module.hot) {
