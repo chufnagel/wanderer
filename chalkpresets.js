65ecb6490a3fa1e110f7262ff8edbd9152bfab71
Charles <charles.hufnagel@protonmail.com>
2018-06-22 16:57:55 -0400

    feat(server/index.js, new packages in package.json, chalkpresets.js): Add barebones server and middl

---
+++ chalkpresets.js
@@ -0,0 +1,16 @@
+const chalk = require("chalk");
+
+// chalk logging
+const log = console.log;
+const chalkSuccess = chalk.bold.green.bgWhite;
+const chalkError = chalk.bold.red.bgBlack;
+const chalkWarning = chalk.underline.orange;
+const chalkInfo = chalk.blue.bgBlack;
+
+module.exports = {
+  log,
+  chalkSuccess,
+  chalkError,
+  chalkWarning,
+  chalkInfo
+};
---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+// const fs = require("fs");
+// const http = require('http');
+// const https = require('https');
+const path = require("path");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const compression = require("compression");
+const logger = require("morgan");
+
+const {
+  log,
+  chalkSuccess,
+  chalkError,
+  chalkWarning,
+  chalkInfo
+} = require("../chalkPresets");
+
+const port = process.env.port || 3000;
+
+const app = express();
+
+// Apply middleware
+app
+  .use(bodyParser.json())
+  .use(cookieParser())
+  .use(compression())
+  .use(logger("dev"))
+  .use(express.static(path.join(__dirname, "../dist/")));
+
+app.listen(port, () => {
+  log(chalkSuccess(`Port ${port} is lit fam 🔥 🔥 🔥`));
+});
