46522827152f1e28f955c64a70010e2307dd1abf
Koichi <s.koichi@gmail.com>
2018-06-26 16:18:49 -0400

    Fixed Merge Conflict

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,109 +6,64 @@ import Destinations from "./Destinations.jsx"
 import BlogList from "./BlogList/BlogList.jsx"
 import CountryPage from "./CountryPage/CountryPage.jsx"
 
-<<<<<<< HEAD
-const Main = props => (
-  <main>
-    <Switch>
-      <Route
-        exact
-        path="/info"
-        render={() => {
-          return <div>Info Page</div>;
-        }}
-      />
+const Main = props => {
+  console.log('main props',props)
+  return (
+    <main>
+      <Switch>
 
-      <Route
-        exact
-        path="/blogs"
-        render={() => {
-          return <div>Blogs Page</div>;
-        }}
-      />
+        <Route exact path='/' render={() => {
+          return (
+            <Home/>
+          )
+        }}/>
 
-      <Route
-        exact
-        path="/photos_videos"
-        render={() => {
+        <Route exact path='/info' render={() => {
+          return (
+            <h4>Info Page</h4>
+          )
+        }}/>
+
+        <Route exact path='/blogs' render={() => {
+          return (
+            <BlogList blogs={props.blogs}/>
+          )
+        }}/>
+
+        <Route exact path='/photos_videos' render={() => {
           return (
             <div>
-              <PhotoGrid travelData={props.travelData} />
+              <PhotoGrid photos={props.photos}/>
             </div>
-          );
-        }}
-      />
+          )
+        }}/>
 
-      <Route
-        exact
-        path="/destinations"
-        render={() => {
-          return <div>Destinations Page</div>;
-        }}
-      />
-    </Switch>
-  </main>
-);
-=======
-const Main = props => {
-    console.log('main props',props)
-    return (
-      <main>
-        <Switch>
+         <Route exact path={`/${props.country}`} render={() =>
+           <CountryPage country={props.country} />}
+         />
 
-          <Route exact path='/' render={() => {
-            return (
-              <Home/>
-            )
-          }}/>
-
-          <Route exact path='/info' render={() => {
-            return (
-              <h4>Info Page</h4>
-            )
-          }}/>
-
-          <Route exact path='/blogs' render={() => {
-            return (
-              <BlogList blogs={props.blogs}/>
-            )
-          }}/>
-
-          <Route exact path='/photos_videos' render={() => {
+        <Route exact path='/destinations' render={() => {
             return (
               <div>
-                <PhotoGrid photos={props.photos}/>
+                <ul>
+                  <h4> places i've been </h4>
+                  <li>Reykjavik, Iceland</li>
+                  <li>Bonquete, Panama</li>
+                  <li>Panama City, Panama</li>
+                  <li>Seattle, USA</li>
+                </ul>
+                <ul>
+                  <h4> places i'd love to go </h4>
+                  <li>New York, New York</li>
+                  <li>Hong Kong, Hong Kong</li>
+                  <li>Sydney, Australia</li>
+                  <li>Argentina</li>
+                </ul>
               </div>
             )
-          }}/>
-
-           <Route exact path={`/${props.country}`} render={() =>
-             <CountryPage country={props.country} />}
-           />
-
-          <Route exact path='/destinations' render={() => {
-              return (
-                <div>
-                  <ul>
-                    <h4> places i've been </h4>
-                    <li>Reykjavik, Iceland</li>
-                    <li>Bonquete, Panama</li>
-                    <li>Panama City, Panama</li>
-                    <li>Seattle, USA</li>
-                  </ul>
-                  <ul>
-                    <h4> places i'd love to go </h4>
-                    <li>New York, New York</li>
-                    <li>Hong Kong, Hong Kong</li>
-                    <li>Sydney, Australia</li>
-                    <li>Argentina</li>
-                  </ul>
-                </div>
-              )
-          }}/>
-        </Switch>
-      </main>
-      )
-    };
->>>>>>> d01da0cf312d602648d3d9b4d2f7389b720b4afe
-
+        }}/>
+      </Switch>
+    </main>
+    )
+  };
 export default Main;
