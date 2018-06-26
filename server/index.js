748bdbd7dbdeceb2032217f0d316c8eda571ef3c
Charles <charles.hufnagel@protonmail.com>
2018-06-25 22:23:05 -0400

    Tweak server file

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 // const fs = require("fs");
 // const http = require('http');
 // const https = require('https');
-const path = require("path");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const compression = require("compression");
@@ -22,13 +21,13 @@ const port = process.env.port || 3000;
 const app = express();
 
 // Apply middleware
-app
-  .use(bodyParser.json())
-  .use(cookieParser())
-  .use(compression())
-  .use(logger("dev"))
-  .use(cors())
-  .use(express.static(path.join(__dirname, "../dist/")));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(compression());
+app.use(logger("dev"));
+app.use(cors());
+// app.use(express.static(path.join(__dirname, "../dist/")));
+app.use(express.static("dist"));
 
 app.listen(port, () => {
   log(chalkSuccess(`Port ${port} is lit fam 🔥 🔥 🔥`));
