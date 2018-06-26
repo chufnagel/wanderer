ae5cc4e54f853ec0bbc0fde2c134f4ce466ef04c
Mandy Mak <mandymak22@gmail.com>
2018-06-26 19:53:04 -0400

    Links on sidebar work. Add Explore component

---
+++ src/components/Explore/Explore.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+const Explore = () => (
+  <div>
+    <Typography variant="Headline" gutterButton>
+      Explore
+    </Typography>
+    <Typography variant="body2" gutterBottom>
+      Oh my god, literally, I love exploring.
+    </Typography>
+  </div>
+);
+
+export default Explore;
