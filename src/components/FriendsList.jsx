f9e53231d23b886d8a507a49bd7be9ff4c63a44a
Mandy Mak <mandymak22@gmail.com>
2018-06-27 13:56:39 -0400

    Implement a MVP version of FriendsList with hardcoded data

--- src/components/FriendsList.jsx
+++ src/components/FriendsList.jsx
@@ -8,19 +8,28 @@ import ListItemText from "@material-ui/core/ListItemText";
 
 // This is roughly what we will use when we have some data in our DB.
 
-const FriendsList = props => (
-  <List>
-    {props.friends.map(friend => (
-      <ListItem key={friend.id} button>
-        <Link to="/profile">
-          {/* dynamically routed */}
-          <ListItemText primary={friend.name} />
-        </Link>
-      </ListItem>
-    ))};
-  </List>
-);
+// const FriendsList = props => (
+//   <List>
+//     {props.friends.map(friend => (
+//       <ListItem key={friend.id} button>
+//         <Link to="/profile">
+//           {/* dynamically routed */}
+//           <ListItemText primary={friend.name} />
+//         </Link>
+//       </ListItem>
+//     ))};
+//   </List>
+// );
 
 // In the meantime, let's hardcode it.
+const FriendsList = () => (
+  <List>
+    <ListItem key={id} button>
+      <Link to="/profile">
+        <ListItemText primary="Lina Luna Lei" />
+      </Link>
+    </ListItem>
+  </List>
+);
 
 export default FriendsList;
