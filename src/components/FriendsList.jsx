21137dc67b8182a553f249fc4be17a207f975639
Mandy Mak <mandymak22@gmail.com>
2018-06-27 12:06:04 -0400

    Implement FriendsList (a version that can be used when we have data in our DB)

---
+++ src/components/FriendsList.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { withStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+
+// This is roughly what we will use when we have some data in our DB.
+
+const FriendsList = props => (
+  <List>
+    {props.friends.map(friend => (
+      <ListItem key={friend.id} button>
+        <Link to="/profile">
+          {/* dynamically routed */}
+          <ListItemText primary={friend.name} />
+        </Link>
+      </ListItem>
+    ))};
+  </List>
+);
+
+// In the meantime, let's hardcode it.
+
+export default FriendsList;
