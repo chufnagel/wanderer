ce63527ce7b94b82eae7f15a22dc87be4edbb955
Mandy Mak <mandymak22@gmail.com>
2018-06-27 14:11:07 -0400

    Add routes to Main.jsx, fix formatting

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,50 +1,83 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import PhotoGrid from "./PhotoGrid.jsx";
-import Home from "./Home.jsx"
-import BlogList from "./BlogList/BlogList.jsx"
-import CountryPage from "./CountryPage/CountryPage.jsx"
-import Destinations from "./Destinations.jsx"
+import PhotoGrid from "./PhotoGrid";
+import Home from "./Home";
+import BlogList from "./BlogList/BlogList";
+import CountryPage from "./CountryPage/CountryPage";
+import Destinations from "./Destinations";
+import Explore from "./Explore/Explore";
+import FriendsList from "./FriendsList";
 
 const Main = props => {
-  console.log('main props',props)
+  console.log("main props", props);
   return (
     <main>
       <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return <Home setNavFlagToDashboard={props.setNavFlagToDashboard} />;
+          }}
+        />
 
-        <Route exact path='/' render={() => {
-          return (
-            <Home setNavFlagToDashboard={props.setNavFlagToDashboard}/>
-          )
-        }}/>
+        <Route
+          exact
+          path="/blogs"
+          render={() => {
+            return (
+              <BlogList
+                blogs={props.blogs}
+                setNavFlagToDashboard={props.setNavFlagToDashboard}
+              />
+            );
+          }}
+        />
 
-        <Route exact path='/blogs' render={() => {
-          return (
-            <BlogList blogs={props.blogs} setNavFlagToDashboard={props.setNavFlagToDashboard}/>
-          )
-        }}/>
-
-        <Route exact path='/photos_videos' render={() => {
-          return (
-             <div>
-              <PhotoGrid photos={props.photos} setNavFlagToDashboard={props.setNavFlagToDashboard}/>
-            </div>
-          )
-        }}/>
-
-         <Route exact path={`/${props.country}`} render={() =>
-           <CountryPage country={props.country} setNavFlagToCountryorCity={props.setNavFlagToCountryorCity} />}
-         />
-
-        <Route exact path='/destinations' render={() => {
+        <Route
+          exact
+          path="/photos_videos"
+          render={() => {
             return (
               <div>
-                <Destinations setNavFlagToDashboard={props.setNavFlagToDashboard}/>
+                <PhotoGrid
+                  photos={props.photos}
+                  setNavFlagToDashboard={props.setNavFlagToDashboard}
+                />
               </div>
-            )
-        }}/>
+            );
+          }}
+        />
+
+        <Route
+          exact
+          path={`/${props.country}`}
+          render={() => (
+            <CountryPage
+              country={props.country}
+              setNavFlagToCountryorCity={props.setNavFlagToCountryorCity}
+            />
+          )}
+        />
+
+        <Route
+          exact
+          path="/destinations"
+          render={() => {
+            return (
+              <div>
+                <Destinations
+                  setNavFlagToDashboard={props.setNavFlagToDashboard}
+                />
+              </div>
+            );
+          }}
+        />
+
+        <Route exact path="/explore" component={Explore} />
+        <Route exact path="/friends" component={FriendsList} />
       </Switch>
     </main>
-    )
-  };
+  );
+};
 export default Main;
