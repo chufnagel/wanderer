0aec23b2fe4aeb05dd94cc11ca963058d9ba794d
Lina Lei <leilsaru@gmail.com>
2018-06-28 17:05:14 -0400

    Points Of Interest sorted in descending rating order; moved Points Of Interest into LocationProfile folder

--- src/components/LocationProfile/PointsOfInterest.jsx
+++ src/components/LocationProfile/PointsOfInterest.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 const PointsOfInterest = (props) => {
+  var sorted = props.pointsOfInterest.sort((a, b) => {return b.rating - a.rating});
   return (
     <div className="points-of-interest-section">
       <h2>Top Places To Visit In {props.location}:</h2>
-      {props.pointsOfInterest.map((place, ind) => (
+      {sorted.map((place, ind) => (
         <div className="point-of-interest" key={ind}>
           <h3>{place.name}</h3>
           <p>Address: {place.formatted_address}</p>
