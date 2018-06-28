09c40bab0d5a0bd980d85dcddc6818114ac9a496
Koichi <s.koichi@gmail.com>
2018-06-28 14:44:44 -0400

    Added sub-titles to Friend component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -14,6 +14,7 @@ const Profile = (props) => {
     <div>
       <img src={"https://pbs.twimg.com/profile_images/585086002466795521/HgNCKU0D_400x400.jpg"} width="33%"/>
       <ul className="profile-image" width="15%" fontSize="150%">25&#x2714;<br/>5&#10084;</ul>
+      <h1>Blogs</h1>
       <ul width="52%"><BlogList setNavFlagToDashboard={props.setNavFlagToDashboard} blogs={props.blogs}/></ul>
       <ul width="35%"></ul>
       <span><PhotoGrid photos={props.photos} setNavFlagToDashboard={props.setNavFlagToDashboard}/></span>
