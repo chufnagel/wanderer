4466741f7a27cfb478d940dbc981aaaae8ac9dd7
Mandy Mak <mandymak22@gmail.com>
2018-06-29 13:57:30 -0400

    Refactor FriendsList as a stateful component to add a lifecycle method

--- src/components/FriendsList.jsx
+++ src/components/FriendsList.jsx
@@ -1,9 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import { withStyles } from "@material-ui/core/styles";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
+// import { withStyles } from "@material-ui/core/styles";
 import ListItemText from "@material-ui/core/ListItemText";
 import Typography from "@material-ui/core/Typography";
 
@@ -22,20 +20,40 @@ import Typography from "@material-ui/core/Typography";
 //   </List>
 // );
 
-// In the meantime, let's hardcode it.
-const FriendsList = () => (
-  <div>
-    <Typography variant="headline">
-      We didn't think you'd have friends, but here they are.
-    </Typography>
-    <div>
-      <center>
-        <Link to="/profile">
-          <ListItemText primary="Lina Luna Lei" />
-        </Link>
-      </center>
-    </div>
-  </div>
-);
+class FriendsList extends Component {
+  componentDidMount() {
+    // function for getting friends
+  }
+
+  render() {
+    return (
+      <List>
+        {props.friends.map(friend => (
+          <ListItem key={friend.id} button>
+            <Link to={`/profile/${friend.id}`}>
+              <ListItemText primary={friend.username} />
+            </Link>
+          </ListItem>
+        ))};
+      </List>
+    );
+  }
+}
+
+// Hard coded FriendsList (from before we had a DB set up)
+// const FriendsList = () => (
+//   <div>
+//     <Typography variant="headline">
+//       We didn't think you'd have friends, but here they are.
+//     </Typography>
+//     <div>
+//       <center>
+//         <Link to="/profile">
+//           <ListItemText primary="Lina Luna Lei" />
+//         </Link>
+//       </center>
+//     </div>
+//   </div>
+// );
 
 export default FriendsList;
