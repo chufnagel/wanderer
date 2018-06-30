c31f307b7b47ee0efee38d77cddc440f15c46473
Koichi <s.koichi@gmail.com>
2018-06-30 18:20:23 -0400

    Cleared out login info from knexfile

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
     connection: {
       host: "127.0.0.1",
       user: "root",
-      password: "Macrostar914*",
+      password: "",
       database: "wanderer"
     },
     useNullAsDefault: true,
