4cf4aab8ed2c91550ced426bf5185b4eae6b6ed2
Mandy Mak <mandymak22@gmail.com>
2018-06-30 15:01:53 -0400

    Adjust formatting of user model

--- server/Models/user.js
+++ server/Models/user.js
@@ -9,34 +9,24 @@ User.findByUserId = userId => {
     .catch(err => console.error(err));
 };
 
-// User.retrieveFriendsByUserId = (userId, cb) => {
-//   console.log('userId',userId)
-//   return db("user_friends")
-//     .where({ user_id: userId })
-//     .then((friends_id) => {
-//       //console.log('friends_id', friends_id)
-//       var arr = friends_id.map(function (el) { return el.friend_id})
-//       console.log('arr',arr)
-//       db("users").whereIn('user_id', arr)
-//       .then((friends_id) => {
-//         console.log('friends_id', friends_id)
-//         cb(friends_id)
-//       })
-//       .catch(err => console.error(err));
-//     })
-//     .catch(err => console.error(err));
-// };
-
 User.retrieveFriendsByUserId = (userId, cb) => {
-  return db("users")
-    .join("user_friends", "users.user_id", "=", "user_friends.user_id")
-    .select("user_friends.friend_id", "users_username")
-    .where("users.user_id", "=", userId)
+  console.log("userId", userId);
+  return db("user_friends")
+    .where({ user_id: userId })
+    .then(friends_id => {
+      let arr = friends_id.map(el => {
+        return el.friend_id;
+      });
+      console.log("arr", arr);
+      db("users")
+        .whereIn("user_id", arr)
+        .then(friends_id => {
+          console.log("friends_id", friends_id);
+          cb(friends_id);
+        })
+        .catch(err => console.error(err));
+    })
+    .catch(err => console.error(err));
 };
 
 module.exports = User;
-
-// /*
-// select * from users where (select * )
-
-// select users_username, user_friends.friend_id inner join users.user_id = user_friends.user_id where users_id = 1
