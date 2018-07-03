87bcd39d7ac3d64523b5f78886df3adf86f2bb9a
Koichi <s.koichi@gmail.com>
2018-07-03 11:56:16 -0400

    Removed unnecessary npm packages

--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ import { BrowserRouter, Route } from "react-router-dom";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 import store from "./store";
-import { ConnectedRouter } from "react-router-redux";
-import { createBrowserHistory } from "history";
-// import {history} from './store.js'
 
 // Replaced BrowserRouter with ConnectedRouter as the former does not seem to work with redux
 render(
