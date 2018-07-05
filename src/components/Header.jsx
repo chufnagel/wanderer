f60789b201dd14cbdaa7fce7a8ec76375c40a966
Mandy Mak <mandymak22@gmail.com>
2018-07-05 02:05:55 -0400

    Refactor Header to show all nav links for now (need to refactor to render depending on the main view (LocationProfile, UserProfile, Home)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,52 +5,77 @@ import ListItemText from "@material-ui/core/ListItemText";
 // The Header creates links that can be used to navigate
 // between routes.
 
-const Header = (props) => {
-  if (props.navFlag === "dashboard") {
-    return (
-      <header>
-        <center>
-          <nav>
-            <Link to="/">
-              <ListItemText primary="home" />
-            </Link>
-            <Link to="/blogs">
-              <ListItemText primary="blog posts" />
-            </Link>
-            <Link to="/photos_videos">
-              <ListItemText primary="photos & videos" />
-            </Link>
-            <Link to="/destinations">
-              <ListItemText primary="destinations" />
-            </Link>
-          </nav>
-        </center>
-      </header>
-    );
-  }
-
-  if (props.navFlag === "countryOrCity") {
-    return (
-      <header>
-        <center>
-          <nav>
-            <Link to="/">
-              <ListItemText primary="information" />
-            </Link>
-            <Link to="/attractions">
-              <ListItemText primary="attractions" />
-            </Link>
-            <Link to="/blogs">
-              <ListItemText primary="blog posts" />
-            </Link>
-            <Link to="/photos_videos">
-              <ListItemText primary="photos & videos" />
-            </Link>
-          </nav>
-        </center>
-      </header>
-    );
-  }
+const Header = () => {
+  return (
+    <header>
+      <center>
+        <nav>
+          <Link to="/">
+            <ListItemText primary="home" />
+          </Link>
+          <Link to="/attractions">
+            <ListItemText primary="attractions" />
+          </Link>
+          <Link to="/blogs">
+            <ListItemText primary="blog posts" />
+          </Link>
+          <Link to="/photos_videos">
+            <ListItemText primary="photos & videos" />
+          </Link>
+          <Link to="/destinations">
+            <ListItemText primary="destinations" />
+          </Link>
+        </nav>
+      </center>
+    </header>
+  );
 };
+
+//   if (props.navFlag === "dashboard") {
+//     return (
+//       <header>
+//         <center>
+//           <nav>
+//             <Link to="/">
+//               <ListItemText primary="home" />
+//             </Link>
+//             <Link to="/blogs">
+//               <ListItemText primary="blog posts" />
+//             </Link>
+//             <Link to="/photos_videos">
+//               <ListItemText primary="photos & videos" />
+//             </Link>
+//             <Link to="/destinations">
+//               <ListItemText primary="destinations" />
+//             </Link>
+//           </nav>
+//         </center>
+//       </header>
+//     );
+//   }
+
+//   if (props.navFlag === "countryOrCity") {
+//     return (
+//       <header>
+//         <center>
+//           <nav>
+//             <Link to="/">
+//               <ListItemText primary="information" />
+//             </Link>
+//             <Link to="/attractions">
+//               <ListItemText primary="attractions" />
+//             </Link>
+//             <Link to="/blogs">
+//               <ListItemText primary="blog posts" />
+//             </Link>
+//             <Link to="/photos_videos">
+//               <ListItemText primary="photos & videos" />
+//             </Link>
+//           </nav>
+//         </center>
+//       </header>
+//     );
+//   }
+// };
 
 export default Header;
