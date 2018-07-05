d21edebc926d82556cfe08ea10367d6d48d1cc7a
Mandy Mak <mandymak22@gmail.com>
2018-07-04 21:06:42 -0400

    Implement getPointsOfInterest and getAttractions action creators (sync and async), rename location in state to selectedLocation

--- src/actions/search.js
+++ src/actions/search.js
@@ -1,7 +1,37 @@
-// import { GET_POINTS_OF_INTEREST, GET_ATTRACTIONS } from "./types";
+import { GET_POINTS_OF_INTEREST, GET_ATTRACTIONS } from "./types";
 
-// export getPointsOfInterest(term) {
-//   return dispatch => {
-//     axios.
-//   }
+export function getPointsOfInterestAsync(pointsOfInterest) {
+  return {
+    type: GET_POINTS_OF_INTEREST,
+    pointsOfInterest
+  };
+}
+
+export function getPointsOfInterest(term) {
+  return dispatch => {
+    axios.post("/getPointsOfInterest", { location: term }).then(({ data }) => {
+      const pointsOfInterest = data.map(attraction => {
+        return attraction;
+      });
+      dispatch(getPointsOfInterestAsync(pointsOfInterest));
+    });
+  };
+}
+
+export function getAttractionsAsync(attractions) {
+  return {
+    type: GET_ATTRACTIONS,
+    attractions
+  };
+}
+
+export function getAttractions(term) {
+  return dispatch => {
+    axios.post("/getAttractions", { location: term }).then(({ data }) => {
+      const attractions = data.map(attraction => {
+        return attraction;
+      });
+      dispatch(getAttractionsAsync(attractions));
+    });
+  };
 }
