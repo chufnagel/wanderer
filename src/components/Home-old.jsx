0e352fa0bae4846eb988ae4da5d7ed4263b17d5e
Mandy Mak <mandymak22@gmail.com>
2018-07-05 10:03:16 -0400

    Create Redux-friendly version of Home component

--- src/components/Home-old.jsx
+++ src/components/Home-old.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Typography from "@material-ui/core/Typography";
+import lifecycle from "react-pure-lifecycle";
 import Stats from "./Stats";
+
+const methods = {
+  componentDidMount(props) {
+    props.setNavFlagToDashboard();
+  }
+};
 
 const recentBlogs = "Your Friends' Popular/Recent blogs";
 const recentPhotos = "Your Friends' Popular/Recent photos";
@@ -12,5 +20,4 @@ const Home = () => (
     <Typography variant="body1">{recentPhotos}</Typography>
   </div>
 );
-
-export default Home;
+export default lifecycle(methods)(Home);
