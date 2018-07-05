08f48a70a50fa0637067dd3272e88c7a4c0617ce
Mandy Mak <mandymak22@gmail.com>
2018-07-05 03:20:07 -0400

    Last attempt of importing SearchContainer not successful. REALLY SHOULD SLEEP NOW

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -1,15 +1,15 @@
 import { connect } from "react-redux";
 import { getPointsOfInterest, getAttractions } from "../actions/search";
-import Search from "../components/Sidebar/Search";
-
-const mapDispatchToProps = dispatch => ({
-  getPointsOfInterest: term => dispatch(getPointsOfInterest(term)),
-  getAttractions: term => dispatch(getAttractions(term))
-});
+import Search from "../components/Search";
 
 const mapStateToProps = state => ({
   pointsOfInterest: state.pointsOfInterest,
   attractions: state.attractions
+});
+
+const mapDispatchToProps = dispatch => ({
+  getPointsOfInterest: term => dispatch(getPointsOfInterest(term)),
+  getAttractions: term => dispatch(getAttractions(term))
 });
 
 const SearchContainer = connect(
