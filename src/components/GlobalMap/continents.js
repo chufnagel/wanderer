a8cbf8c15b5dde6cfe45dfb663ac662781d07dc2
Mandy Mak <mandymak22@gmail.com>
2018-07-06 20:34:56 -0400

    Add list of continents to color-coordinate GlobalMap (it is currently grouped by subregion of the world)

---
+++ src/components/GlobalMap/continents.js
@@ -0,0 +1,12 @@
+const continents = [
+  "Asia",
+  "Africa",
+  "North America",
+  "South America",
+  "Antarctica",
+  "Europe",
+  "Oceania",
+  "Seven seas (open ocean)"
+];
+
+export default continents;
