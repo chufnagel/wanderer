241dec03f5fc9a470d3990131f79134aa114654c
Mandy Mak <mandymak22@gmail.com>
2018-07-07 14:20:07 -0400

    Change /destinationsFuture and /destinationsPast to /favorites and /visited in server/routes.js

--- server/routes.js
+++ server/routes.js
@@ -31,21 +31,21 @@ router.post("/getPointsOfInterest", (req, res) => {
 
 // calls the helper function to query REST Countries API for basic info for given location
 router.post("/getLocationBasicInfo", (req, res) => {
-  getLocationBasicInfo(req.body.location, (data) => {
+  getLocationBasicInfo(req.body.location, data => {
     if (data) res.send(data);
     // else (err) console.log("error getting basic info from server", err);
   });
 });
 
 // User.retrieveUserByUserId
-router.get("/destinationsFuture", (req, res) => {
+router.get("/favorites", (req, res) => {
   Destinations.retrieveFavByUserId(req.query.user_id, countries => {
     // console.log('favorite countries', countries)
     res.send(countries);
   });
 });
 
-router.get("/destinationsPast", (req, res) => {
+router.get("/visited", (req, res) => {
   Destinations.retrieveVisitedByUserId(req.query.user_id, countries => {
     // console.log('visited countries', countries)
     res.status(200).send(countries);
@@ -55,14 +55,14 @@ router.get("/destinationsPast", (req, res) => {
   });
 });
 
-router.post("/destinationsFuture", (req, res) => {
-  console.log(req.body)
-  Destinations.addFavByUserId(req.body.user_id, req.body.country)
+router.post("/favorites", (req, res) => {
+  console.log(req.body);
+  Destinations.addFavByUserId(req.body.userId, req.body.country);
 });
 
-router.post("/destinationsPast", (req, res) => {
-  console.log(req.body)
-  Destinations.addVisitedByUserId(req.body.user_id, req.body.country)
+router.post("/visited", (req, res) => {
+  console.log(req.body);
+  Destinations.addVisitedByUserId(req.body.user_id, req.body.country);
 });
 
 router.get("/friends", (req, res) => {
@@ -73,7 +73,7 @@ router.get("/friends", (req, res) => {
   }).catch(err => console.error(err));
 });
 
-/* KS: temporarily commenting out everything below as "async" does not run on HR pairing station 
+/* KS: temporarily commenting out everything below as "async" does not run on HR pairing station
 
 router.get("/userInfo", async (req, res, next) => {
   console.log(req.params);
