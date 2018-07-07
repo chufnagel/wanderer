4f2037b7bd5dd85a262b1cd1a6718be440ff9193
Mandy Mak <mandymak22@gmail.com>
2018-07-07 15:28:27 -0400

    Make updates to imports to SearchContainer and root reducer in light of having moved the locationBasicInfo actions and reducers. FriendsList and Destinations are still WIP's. (App will crash.)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,14 @@
-import { combineReducers } from 'redux';
-import photoReducer from "./photoReducer.js"
-import navReducer from "./navReducer.js"
-import {reducerPast, reducerFuture} from "./destinationsReducer.js"
+import { combineReducers } from "redux";
+import { faveDestinations, visitedDestinations } from "./destinationsReducer";
 import blogs from "./blogReducer";
 import location from "./locationReducer";
-import locationBasicInfo from "./locationBasicInfoReducer";
 import headerSetting from "./headerReducer";
-import { pointsOfInterest, attractions } from "./searchReducer";
+import {
+  pointsOfInterest,
+  attractions,
+  locationBasicInfo
+} from "./searchReducer";
 // import photoReducer from "./photoReducer"; -- currently an empty reducer
-// import navReducer from "./navReducer";
 
 // use map, filter, Object.assign() and array destructuring to
 // return new version of state!
@@ -20,24 +20,11 @@ const rootReducer = combineReducers({
   attractions,
   pointsOfInterest,
   blogs,
-  headerSetting
+  headerSetting,
+  faveDestinations,
+  visitedDestinations
   // photos
-  // destinations
   // userId
 });
 
 export default rootReducer;
-
-// photos: photoReducer,
-// navFlag: navReducer,
-// Below are placeholders and need to be updated. A reducer needs to be defined for each item in the store or else it will throw an error
-// currentText: (state = {}) => state,
-// countries: (state = {}) => state,
-// location: (state = {}) => state,
-// photos: (state = {}) => state,
-// destinations: (state = {}) => state
-// blogs: blogReducer,
-// photos: photoReducer,
-// navFlag: navReducer,
-// destinationsFuture: reducerPast, 
-// destinationsPast: reducerFuture,
