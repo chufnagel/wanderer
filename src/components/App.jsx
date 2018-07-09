d9e1ef3a9a5d3636c7464e5ae1282a28b3131c09
Mandy Mak <mandymak22@gmail.com>
2018-07-09 14:22:04 -0400

    Import GlobalMapContainer in lieu of GlobalMap in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { hot } from "react-hot-loader";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Typography from "@material-ui/core/Typography";
 import Sidebar from "./Sidebar/Sidebar";
-import GlobalMap from "./GlobalMap/GlobalMap"; // using the presentation component for now, as we figure out the parts of state it needs
+import GlobalMapContainer from "../containers/GlobalMapContainer";
 import Home from "./Home";
 import HeaderContainer from "../containers/HeaderContainer";
 import UserProfile from "./UserProfile/UserProfile";
@@ -24,7 +24,7 @@ const App = () => (
     <center>
       <Typography variant="display2">Wanderer</Typography>
       <br />
-      <GlobalMap />
+      <GlobalMapContainer />
     </center>
     <Sidebar />
     <center>
