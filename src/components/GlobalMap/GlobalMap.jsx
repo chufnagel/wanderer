d94c65c65c1e8bb5b1f6b9f48a7bd54c29840e5a
Mandy Mak <mandymak22@gmail.com>
2018-07-09 12:02:49 -0400

    GlobalMap is now colorcoded by continent. Tweaked color settings for hover and pressed modes as well

--- src/components/GlobalMap/GlobalMap.jsx
+++ src/components/GlobalMap/GlobalMap.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 import Tooltip from "@material-ui/core/Tooltip";
 import chroma from "chroma-js";
-import subregions from "./subregions";
+import continents from "./continents";
 
 const wrapperStyles = {
   width: "100%",
@@ -10,14 +10,19 @@ const wrapperStyles = {
   margin: "0 auto"
 };
 
-const colorScale = chroma
-  .scale(["b6fbea", "ccccff", "663894"]) // icy teal, pastel lavender, dark purple
-  .mode("lch")
-  .colors(24);
-
+const continentColors = [
+  chroma("F4C8D9"), // queen pink // Asia
+  chroma("BCFF93"), // mint green // Africa
+  chroma("DBCBF4"), // pale lavender // North America
+  chroma("79E0C8"), // pearl aqua // South America
+  chroma("9BE6F1"), // winter wizard (icy blue) // Antarctica
+  chroma("9BBEFF"), // baby blue eyes // Europe
+  chroma("FFEBBC"), // lemon meringue // Oceania
+  chroma("FFEBBC") // white // black // Seven seas (open oceans)
+];
 class GlobalMap extends Component {
   state = {
-    subregions,
+    continents,
     selectedCountry: ""
   };
 
@@ -46,43 +51,43 @@ class GlobalMap extends Component {
             {(geographies, projection) =>
               geographies.map((geography, i) => (
                 <Tooltip
-                  key={i}
+                  key={geographies[i]}
                   id="tooltip-country"
                   title={geography.properties.name}
                   placement="top-start"
                   onOpen={e => this.changeSelectedCountry(e)}
+                  onClose={() => this.changeSelectedCountry(null)}
                 >
                   <Geography
-                    key={i}
                     geography={geography}
                     projection={projection}
-                    onClick={this.handleClick}
+                    onClick={e => this.handleClick(e)}
                     style={{
                       default: {
                         fill:
-                          colorScale[
-                            subregions.indexOf(geography.properties.subregion)
+                          continentColors[
+                            continents.indexOf(geography.properties.continent)
                           ],
                         stroke: "#607D8B",
                         strokeWidth: 0.75,
                         outline: "none"
                       },
                       hover: {
-                        fill: chroma(
-                          colorScale[
-                            subregions.indexOf(geography.properties.subregion)
-                          ]
-                        ).darken(0.5),
+                        fill: continentColors[
+                          continents.indexOf(geography.properties.continent)
+                        ]
+                          .brighten(0.5)
+                          .saturate(0.3),
                         stroke: "#607D8B",
                         strokeWidth: 0.75,
                         outline: "none"
                       },
                       pressed: {
-                        fill: chroma(
-                          colorScale[
-                            subregions.indexOf(geography.properties.subregion)
-                          ]
-                        ).brighten(0.5),
+                        fill: continentColors[
+                          continents.indexOf(geography.properties.continent)
+                        ]
+                          .darken()
+                          .saturate(0.5),
                         stroke: "#607D8B",
                         strokeWidth: 0.75,
                         outline: "none"
