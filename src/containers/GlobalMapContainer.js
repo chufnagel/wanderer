d887078ed652a3373fc58cda90932616ce4dec1f
Mandy Mak <mandymak22@gmail.com>
2018-07-09 14:19:33 -0400

    Add actions and parts of state to GlobalMapContainer

--- src/containers/GlobalMapContainer.js
+++ src/containers/GlobalMapContainer.js
@@ -1,15 +1,33 @@
 import { connect } from "react-redux";
-import { addVisitedDestination } from "../actions/destinations";
+import changeSelectedLocation from "../actions/location";
+import {
+  getPointsOfInterest,
+  getAttractions,
+  getLocationBasicInfo
+} from "../actions/search";
+// import {
+//   addVisitedDestination,
+//   addFaveDestination
+// } from "../actions/destinations";
 import GlobalMap from "../components/GlobalMap/GlobalMap";
 
-const mapStateToProps = ({ state }) => {
+const mapStateToProps = state => {
   return {
-    visitedDestinations: state.visitedDestinations
+    location: state.location,
+    pointsOfInterest: state.pointsOfInterest,
+    attractions: state.attractions
+    // visitedDestinations: state.visitedDestinations,
+    // faveDestinations: state.faveDestinations
   };
 };
 
 const mapDispatchToProps = dispatch => ({
-  addVisitedDestination: country => dispatch(addVisitedDestination(country))
+  changeSelectedLocation: term => dispatch(changeSelectedLocation(term)),
+  getPointsOfInterest: term => dispatch(getPointsOfInterest(term)),
+  getAttractions: term => dispatch(getAttractions(term)),
+  getLocationBasicInfo: term => dispatch(getLocationBasicInfo(term))
+  // addVisitedDestination: term => dispatch(addVisitedDestination(term)),
+  // addFaveDestination: term => dispatch(addFaveDestination(term))
 });
 
 export default connect(
