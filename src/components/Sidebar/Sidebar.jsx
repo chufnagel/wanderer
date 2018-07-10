537c8ffe4fd48412a06f847d2bde76e1cace842b
Mandy Mak <mandymak22@gmail.com>
2018-07-09 22:27:46 -0400

    The header setting will be changed accordingly for every button that is clicked on Sidebar. PropTypes have been added for the Sidebar component

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
@@ -7,49 +8,55 @@ import ListItemText from "@material-ui/core/ListItemText";
 import SearchContainer from "../../containers/SearchContainer";
 import { HOME, LOCATION_PROFILE } from "../../constants";
 
-const Sidebar = () => {
+const Sidebar = props => {
   const { setHeader } = props;
-  <div>
-    <Drawer
-      variant="permanent"
-      position="absolute"
-      className="docked"
-      anchor="left"
-    >
-      <br />
-      <br />
-      <br />
-      <List component="nav">
-        <center>
-          <img width="128px" height="128px" alt="Profile pic" />
-        </center>
+  return (
+    <div>
+      <Drawer
+        variant="permanent"
+        position="absolute"
+        className="docked"
+        anchor="left"
+      >
         <br />
-        <SearchContainer />
         <br />
-        <ListItem button onClick={() => setHeader(HOME)}>
-          <Link to="/">
-            <ListItemText primary="Home" />
-          </Link>
-        </ListItem>
-        <ListItem button>
-          <Link to="/friends" onClick={() => setHeader(HOME)}>
-            <ListItemText primary="Friends" />
-          </Link>
-        </ListItem>
-        <ListItem button>
-          <Link to="/profile" onClick={() => setHeader(HOME)}>
-            <ListItemText primary="Profile" />
-          </Link>
-        </ListItem>
-        <ListItem button onClick={() => setHeader(LOCATION_PROFILE)}>
-          <Link to="/explore">
-            <ListItemText primary="Explore" />
-          </Link>
-        </ListItem>
         <br />
-      </List>
-    </Drawer>
-  </div>
-);
+        <List component="nav">
+          <center>
+            <img width="128px" height="128px" alt="Profile pic" />
+          </center>
+          <br />
+          <SearchContainer />
+          <br />
+          <ListItem button onClick={() => setHeader(HOME)}>
+            <Link to="/">
+              <ListItemText primary="Home" />
+            </Link>
+          </ListItem>
+          <ListItem button>
+            <Link to="/friends" onClick={() => setHeader(HOME)}>
+              <ListItemText primary="Friends" />
+            </Link>
+          </ListItem>
+          <ListItem button>
+            <Link to="/profile" onClick={() => setHeader(HOME)}>
+              <ListItemText primary="Profile" />
+            </Link>
+          </ListItem>
+          <ListItem button onClick={() => setHeader(LOCATION_PROFILE)}>
+            <Link to="/explore">
+              <ListItemText primary="Explore" />
+            </Link>
+          </ListItem>
+          <br />
+        </List>
+      </Drawer>
+    </div>
+  );
+};
+
+Sidebar.propTypes = {
+  setHeader: PropTypes.func.isRequired
+};
 
 export default Sidebar;
