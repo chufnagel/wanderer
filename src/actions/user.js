8ba269c3c29c747c9c693a3379cefd0cd5c43d96
Mandy Mak <mandymak22@gmail.com>
2018-07-10 17:04:43 -0400

    Add getUserInfo actions (request, success, and failure cases)

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,3 +1,41 @@
 import axios from "axios";
-import * as actions from "../types";
+import * as actions from "./types";
 
+function getUserInfoRequest() {
+  return {
+    type: actions.GET_USER_INFO_REQUEST
+  };
+}
+function getUserInfoSuccess(userInfo) {
+  return {
+    type: actions.GET_USER_INFO_SUCCESS,
+    payload: userInfo
+  };
+}
+function getUserInfoFailure(err) {
+  return {
+    type: actions.GET_USER_INFO_FAILURE,
+    err
+  };
+}
+
+export default function getUserInfo(userId) {
+  return dispatch => {
+    dispatch(getUserInfoRequest());
+    axios
+      .get("/getUserInfo", {
+        params: {
+          userId
+        }
+      })
+      .then(({ data }) => {
+        const userInfo = data.map(info => {
+          return info;
+        });
+        dispatch(getUserInfoSuccess(userInfo));
+      })
+      .catch(err => {
+        dispatch(getUserInfoFailure(err));
+      });
+  };
+}
