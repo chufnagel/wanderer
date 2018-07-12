72b7e3ae3515e130b4a781599c7e254e7ee2c978
Charles <charles.hufnagel@protonmail.com>
2018-07-12 16:10:15 -0400

    Periodic save of work. Remove package-lock.json from git cache. Install jsonwebtoken for authentication

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -52,7 +52,7 @@ const Sidebar = ({
               primary="Friends"
               onClick={() => {
                 setHeader(HOME);
-                getFriendsList(userInfo.name);
+                getFriendsList(userInfo.user_id);
               }}
             />
           </Link>
