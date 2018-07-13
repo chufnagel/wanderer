30e507a7cdd67456842866b5fd894fc24a48920d
Charles <charles.hufnagel@protonmail.com>
2018-07-13 09:34:15 -0400

    Remove authentication dependencies and add production logic

--- server/Models/user.js
+++ server/Models/user.js
@@ -1,5 +1,5 @@
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcrypt");
+// const jwt = require("jsonwebtoken");
 const db = require("../db");
 
 const User = {};
